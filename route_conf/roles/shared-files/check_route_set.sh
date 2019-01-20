1e543da16fc0e2805a6ea8a3a32757f81b76f6f7
AlexSergEdge <brink-aleksandr@yandex.ru>
2019-01-20 10:34:12 +0000

    Added shared files and test for delete route role

---
+++ route_conf/roles/shared-files/check_route_set.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+set -o pipefail
+
+IP_TABLE="$(route -n)"
+ROUTE="192.168.3.0     192.168.122.1   255.255.255.0   UG    0      0        0 virbr0"
+if echo "$IP_TABLE" | grep -q "$ROUTE"; then
+    echo "Route found";
+else
+    echo "Route not found";
+fi
--- route_conf/start_on_all_hosts.sh
+++ route_conf/start_on_all_hosts.sh
@@ -6,7 +6,7 @@ HOSTGROUP1="local"
 HOSTGROUP2="remote_vms"
 
 # Запустить на локальном хосте
-ansible-playbook play.yml -i inventory/inventory --extra-vars "host_var='${HOSTGROUP1}'"
+# ansible-playbook play.yml -i inventory/inventory --extra-vars "host_var='${HOSTGROUP1}'"
 
 # Запустить на удаленном хосте
 ansible-playbook play.yml -i inventory/inventory --extra-vars "host_var='${HOSTGROUP2}'"
